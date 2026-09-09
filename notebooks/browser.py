9bd905cff3eecf25f574462a3bdfc2dd0bc76d75
Sam Hames <s.hames@uq.edu.au>
2026-09-09 10:01:15 +1000

    Layout basic source files for the actual browser notebook to run in binderhub

---
+++ notebooks/browser.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+#
+#
+#
+#
+#
+#
+#
+
+# %%
+from ui import UI
+
+UI()
